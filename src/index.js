ef6b488b250a44302214e53d02e712e48b36bb96
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-11 11:01:05 -0800

    added firestore to project and updated index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+import { firestoreReducer } from 'redux-firestore';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterFoodList: foodListReducer,
+  firestore: firestoreReducer
+});
+
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
