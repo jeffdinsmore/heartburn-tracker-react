ce70620f8068e9986facaaafe4d92dde5f054bbf
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-17 22:21:42 -0800

    added components for signup and password reset

---
+++ src/components/Signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+import firebase from "firebase/app";
+
+function Signup() {
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
+      alert("successfully signed up!");
+    }).catch(function (error) {
+      alert(error.message);
+    });
+  }
+}
