c70dcde2a93285c30a38789374d620b5cbb4944a
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-22 09:36:39 -0800

    installing bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
+import Navbar from 'react-bootstrap/Navbar'
+
+
+
 
 const HeartburnHeader = styled.h1`
   font-size: 32px;
@@ -14,8 +18,32 @@ function Header() {
   return (
     <React.Fragment>
       <HeartburnHeader>
-        <h1> Heartburn Tracker</h1>
+        <h1>Heartburn Tracker</h1>
       </HeartburnHeader>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
       <ul>
         <li>
           <Link to="/">Home</Link>
