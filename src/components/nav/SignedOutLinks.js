30609ef5fa8d36dab6ac7a0e472d3c52f7f2c733
JeffDinsmore <hello@jeffdinsmore.com>
2022-01-20 13:30:09 -0800

    updated nav to have login first and signup second

--- src/components/nav/SignedOutLinks.js
+++ src/components/nav/SignedOutLinks.js
@@ -8,8 +8,8 @@ function SignedOutLinks() {
       <Nav className="mr-auto">
       </Nav>
       <Nav>
-        <Nav.Link as={Link} to="/signup">Sign up</Nav.Link>
         <Nav.Link as={Link} to="/login">Login</Nav.Link>
+        <Nav.Link as={Link} to="/signup">Sign up</Nav.Link>
         <Nav.Link to="/" className='btn btn-warning p-1 rounded-circle btn-sm'>
           Not Signed In
         </Nav.Link>
